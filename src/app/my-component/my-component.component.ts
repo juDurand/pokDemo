be36566a41893b514dfb12c9af12c1248c56a240
juldurand <jules.durand@etudiant.univ-ubs.fr>
2021-03-19 14:27:41 +0100

    Q1 to 7, all

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent implements OnInit {
+
+    id:string = '';
+    selectedPokeId:string = '1';
+
+    pokeList : Pokemon[] = [];
+
+    constructor() {
+        this.pokeList.push(new Pokemon('1', 'pikachu'));
+        this.pokeList.push(new Pokemon('2', 'bulbasaur'));
+        this.pokeList.push(new Pokemon('3', 'ivysaur'));
+        this.pokeList.push(new Pokemon('4', 'venusaur'));
+    }
+
+    ngOnInit(): void {
+    }
+
+}
